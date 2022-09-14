e7de05c63799d12645ac4f692cc602c42c07650b
haleyalwayshappy <hongniest@gmail.com>
2022-09-14 13:06:14 +0900

    todo list > register

---
+++ projecr01_2022/src/main/java/todo/TodoRegisterController.java
@@ -0,0 +1,28 @@
+package todo;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name= "todoRegisterController", urlPatterns = "/todo/register")
+public class TodoRegisterController extends HttpServlet {
+    
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("입력화면을 볼 수 있도록 구성 ");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/todo/register.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("입력을 처리하고 목록 페이지로 이동!");
+		response.sendRedirect("/todo/list");
+	}
+
+}
---
+++ projecr01_2022/src/main/webapp/WEB-INF/todo/register.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/todo/register" method="post">
+		<button type="submit">등록처</button>
+	</form>
+</body>
+</html>
